c59873b16abac1f16f437938ce47b867af3851f1
willian-kaminski <developer.williansantos@gmail.com>
2021-05-02 21:48:03 -0300

    add service-admin

---
+++ service-admin/src/test/java/com/kaminski/store/admin/ServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminski.store.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
